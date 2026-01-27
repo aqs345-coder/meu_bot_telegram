ec4489b1d027af14671a04d5b46e5f16c1019ebc
aqs_coder <aqsuel345@gmail.com>
2026-01-26 22:44:25 -0300

    Primeiro teste do bot

--- src/main.py
+++ src/main.py
@@ -1,45 +1,23 @@
-# flake8: noqa: E501
+import os
 
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(level=logging.INFO)
+load_dotenv()
 
-TOKEN = 'YOUR_TOKEN_HERE'
-
-
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text='Olá, eu sou o seu bot de tarefas!')
+TOKEN = os.getenv('TOKEN')
 
 
-def add_task(update, context):
-    task = ' '.join(context.args)
-    with open('tasks.txt', 'a') as file:
-        file.write(task + '\n')
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text='Tarefa adicionada com sucesso!')
-
-
-def list_tasks(update, context):
-    with open('tasks.txt', 'r') as file:
-        tasks = file.readlines()
-    context.bot.send_message(chat_id=update.effective_chat.id, text='\n'.join(
-        [task.strip() for task in tasks]))
-
-
-def main():
-    updater = Updater(TOKEN, use_context=True)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('add', add_task))
-    dp.add_handler(CommandHandler('list', list_tasks))
-
-    updater.start_polling()
-    updater.idle()
-
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Olá! Sou seu novo assistente. O que vamos programar hoje?")
 
 if __name__ == '__main__':
-    main()
+    # Substitua 'SEU_TOKEN_AQUI' pelo token do BotFather
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    app.add_handler(start_handler)
+
+    print("Bot em execução...")
+    app.run_polling()
